ef7042c977cc5c51ae7418c6cdb3c4ef61a4c0c2
Marc <iammarc.0468@gmail.com>
2021-04-20 18:52:50 +0900

    Initial commit

---
+++ src/popup.js
@@ -0,0 +1,44 @@
+document.addEventListener("DOMContentLoaded", function () {
+  var entryElement = document.getElementById("copy");
+  entryElement.addEventListener("click", function () {
+    copyToClipboard();
+  });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  var entryElement = document.getElementById("generate");
+  entryElement.addEventListener("click", function () {
+    var password_form = document.getElementById("password");
+    password_form.value = getRandomStr(document.forms.form_password);
+  });
+});
+
+function getRandomStr(option) {
+  const PASSWORD_LEN = option["length"].value;
+  let SOURCE = "";
+  let result = "";
+
+  if (option["number"].checked) {
+    SOURCE += "0123456789";
+  }
+  if (option["symbol"].checked) {
+    SOURCE += "!#$%&=~/*-+";
+  }
+  if (option["low_character"].checked) {
+    SOURCE += "abcdefghijklmnopqrstuvwxyz";
+  }
+  if (option["upper_character"].checked) {
+    SOURCE += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  }
+  for (var i = 0; i < PASSWORD_LEN; i++) {
+    result += SOURCE[Math.floor(Math.random() * SOURCE.length)];
+  }
+
+  return result;
+}
+
+function copyToClipboard() {
+  var password = document.getElementById("password");
+  password.select();
+  document.execCommand("Copy");
+}
